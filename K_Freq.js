a20509d7ace92d91ed1ff396d9383315cc4567ab
Keshav6918 <keshav_bhardwaj@epam.com>
2025-01-21 11:24:31 +0530

    Update K_Freq.js

--- K_Freq.js
+++ K_Freq.js
@@ -1,1 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    let arr = [];
+    let mp = new Map();
+
+    for(let num of nums){
+        mp.set(num, (mp.get(num) || 0) + 1);
+    }
+
+    let Freq = [];
+
+    for(let [key, value] of mp) {
+        Freq.push([value, key]);
+    }
+    Freq.sort((a, b) => b[0] - a[0]);
+
+    for(let i = 0; i < Freq.length && k !== 0; i++) {
+        arr.push(Freq[i][1]);
+        k--;
+    }
+
+    return arr;
+};
+
 
