89beec607445566f035754ac2286136694ce3a87
Keshav6918 <keshav_bhardwaj@epam.com>
2025-01-24 10:17:51 +0530

    Update WordwithVowels.js

--- WordwithVowels.js
+++ WordwithVowels.js
@@ -1,1 +1,18 @@
+function WordWithVowels(str){
+  const vowels = [aeiouAEIOU]/g;
+  const words = str.split(" ");
 
+  let maximumVowelC = 0;
+  let WordWM = "";
+
+  words.forEach((word) => {
+    const vowelCount = (word.match(vowels) || []).length;
+
+    if(vowelCount > maximumVowelC){
+      maximumVowelC = vowelCount;
+      WordWM = word;
+    }
+  });
+
+  return WordWM;
+}
