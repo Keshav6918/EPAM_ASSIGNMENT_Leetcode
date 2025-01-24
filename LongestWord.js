c7c6d9cab831b64942b9dc97f02d3c71d7e627a4
Keshav6918 <keshav_bhardwaj@epam.com>
2025-01-24 09:49:39 +0530

    Update LongestWord.js

--- LongestWord.js
+++ LongestWord.js
@@ -1,1 +1,8 @@
+function LongestWord(str){
+  
+  str = str.split(" "); // .split() method is uses to divide the string into array of substrings.
 
+  return str.sort((a,b) => b.length - a.length)[0]; // This will return the first element of the array which will be longest as the array is sorted in a descending order.
+}
+
+console.log(LongestWord("Hey my name is Keshav")) // output: Keshav
